f35db8872e062bfd91e24cc3eb70bef539972758
siddhi.shinde <siddhi.shnde@tekditechnologies.com>
2024-12-12 17:46:05 +0530

    App 1

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  const component = `
+    export default function ${req.query.name || 'Component'}() {
+      return <div>Hello from App Component</div>;
+    }
+  `;
+  res.json({ component });
 }
